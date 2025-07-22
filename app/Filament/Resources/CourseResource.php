837ac54576f7529f4c955de38f69bd9370185acb
anani anatide <ananianatid@gmail.com>
2025-07-22 15:31:05 +0000

    Ajout du groupe de navigation "Done" à la ressource Course pour une meilleure organisation dans l'interface Filament.

--- app/Filament/Resources/CourseResource.php
+++ app/Filament/Resources/CourseResource.php
@@ -18,6 +18,7 @@ class CourseResource extends Resource
     protected static ?string $model = Course::class;
 
     protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationGroup = 'Done';
 
     public static function form(Form $form): Form
     {
